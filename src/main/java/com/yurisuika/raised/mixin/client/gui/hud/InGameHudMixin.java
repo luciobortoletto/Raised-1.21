5a27506d26661d0478d0b2c64a16f89fa91b2f5e
yurisuika <yurisuika@protonmail.com>
2023-01-11 18:38:33 -0800

    Refactor + Commands + Inventory Profiles Next Support

--- src/main/java/com/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/com/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
@@ -10,7 +10,7 @@ public class InGameHudMixin {
 
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"), index = 2)
     private int modifyHotbar(int value) {
-        return value - Raised.getDistance();
+        return value - Raised.getHud();
     }
 
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V", ordinal = 1), index = 6)
@@ -20,47 +20,47 @@ public class InGameHudMixin {
 
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;renderHotbarItem(IIFLnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/item/ItemStack;I)V"), index = 1)
     private int modifyItem(int value) {
-        return value - Raised.getDistance();
+        return value - Raised.getHud();
     }
 
     @ModifyVariable(method = "renderMountJumpBar", at = @At(value = "STORE"), ordinal = 3)
     private int modifyJumpBar(int value) {
-        return value - Raised.getDistance();
+        return value - Raised.getHud();
     }
 
     @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/InGameHud;drawTexture(Lnet/minecraft/client/util/math/MatrixStack;IIIIII)V"), index = 2)
     private int modifyExperienceBar(int value) {
-        return value - Raised.getDistance();
+        return value - Raised.getHud();
     }
 
     @ModifyArg(method = "renderExperienceBar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/font/TextRenderer;draw(Lnet/minecraft/client/util/math/MatrixStack;Ljava/lang/String;FFI)I"), index = 3)
     private float modifyXpText(float value) {
-        return value - (float)Raised.getDistance();
+        return value - (float)Raised.getHud();
     }
 
     @ModifyVariable(method = "renderHeldItemTooltip", at = @At(value = "STORE"), ordinal = 2)
     private int modifyHeldItemTooltip(int value) {
-        return value - Raised.getDistance();
+        return value - Raised.getHud();
     }
 
     @ModifyVariable(method = "renderStatusBars", at = @At(value = "STORE"), ordinal = 5)
     private int modifyStatusBars(int value) {
-        return value - Raised.getDistance();
+        return value - Raised.getHud();
     }
 
     @ModifyVariable(method = "renderMountHealth", at = @At(value = "STORE"), ordinal = 2)
     private int modifyMountHealth(int value) {
-        return value - Raised.getDistance();
+        return value - Raised.getHud();
     }
 
     @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;translate(DDD)V", ordinal = 0), index = 1)
     private double modifyActionbar(double value) {
-        return value - (double)Raised.getDistance();
+        return value - (double)Raised.getHud();
     }
 
     @ModifyArg(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;translate(DDD)V", ordinal = 2), index = 1)
     private double modifyChat(double value) {
-        return value - (double)Raised.getDistance() - (double)Raised.getOffset();
+        return value - (double)Raised.getHud() - (double)Raised.getChat();
     }
 
 }
