ad0d545af2123aac903e7d61f623c4662a3671e1
yurisuika <yurisuika@protonmail.com>
2023-02-02 21:21:05 -0800

    Added Pseudo annotations

--- src/main/java/dev/yurisuika/raised/mixin/mods/HealthOverlayMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/mods/HealthOverlayMixin.java
@@ -3,12 +3,14 @@ package dev.yurisuika.raised.mixin.mods;
 import dev.yurisuika.raised.Raised;
 import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import terrails.healthoverlay.HeartRenderer;
 
 public class HealthOverlayMixin {
 
+    @Pseudo
     @Mixin(HeartRenderer.class)
     public static class HeartRendererMixin {
 
