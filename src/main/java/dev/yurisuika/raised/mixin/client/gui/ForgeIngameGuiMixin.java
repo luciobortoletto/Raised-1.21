8d0feb6ba9fb4356e2bd2fc3a665c4ad579f5be9
yurisuika <yurisuika@protonmail.com>
2023-02-02 18:30:57 -0800

    Forge Backport + Forge Icon Location Revert

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/ForgeIngameGuiMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/ForgeIngameGuiMixin.java
@@ -2,23 +2,25 @@ package dev.yurisuika.raised.mixin.client.gui;
 
 import dev.yurisuika.raised.Raised;
 import net.minecraftforge.client.gui.ForgeIngameGui;
-import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(value = ForgeIngameGui.class, priority = -1)
 public class ForgeIngameGuiMixin {
 
-    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;right_height:I", opcode = Opcodes.PUTFIELD))
-    private void redirectRight(ForgeIngameGui instance, int value) {
-        instance.right_height = value + Raised.getHud();
-    }
+    @Shadow
+    public static int left_height = 39 + Raised.getHud();
+    @Shadow
+    public static int right_height = 39 + Raised.getHud();
 
-    @Redirect(method = "render", at = @At(value = "FIELD", target = "Lnet/minecraftforge/client/gui/ForgeIngameGui;left_height:I", opcode = Opcodes.PUTFIELD))
-    private void redirectLeft(ForgeIngameGui instance, int value) {
-        instance.left_height = value + Raised.getHud();
+    @Inject(method = "render", at = @At("TAIL"))
+    public void injectHeight(CallbackInfo ci) {
+        right_height = 39 + Raised.getHud();
+        left_height = 39 + Raised.getHud();
     }
 
     @ModifyVariable(method = "renderChat", at = @At(value = "HEAD"), ordinal = 1, argsOnly = true, remap = false)
@@ -28,6 +30,26 @@ public class ForgeIngameGuiMixin {
 
     @ModifyVariable(method = "renderRecordOverlay", at = @At(value = "HEAD"), ordinal = 1, argsOnly = true, remap = false)
     private int modifyActionbar(int value) {
+        return value - Raised.getHud();
+    }
+
+    @ModifyVariable(method = "renderFood", at = @At(value = "HEAD"), ordinal = 1, argsOnly = true, remap = false)
+    private int modifyFood(int value) {
+        return value - Raised.getHud();
+    }
+
+    @ModifyVariable(method = "renderHealth", at = @At(value = "HEAD"), ordinal = 1, argsOnly = true, remap = false)
+    private int modifyHealth(int value) {
+        return value - Raised.getHud();
+    }
+
+    @ModifyVariable(method = "renderAir", at = @At(value = "HEAD"), ordinal = 1, argsOnly = true, remap = false)
+    private int modifyAir(int value) {
+        return value - Raised.getHud();
+    }
+
+    @ModifyVariable(method = "renderHealthMount", at = @At(value = "HEAD"), ordinal = 1, argsOnly = true, remap = false)
+    private int modifyHealthMount(int value) {
         return value - Raised.getHud();
     }
 
