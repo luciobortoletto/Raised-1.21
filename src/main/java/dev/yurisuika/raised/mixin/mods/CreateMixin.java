e488f3f0cbb5dc279806cf0635d7b4650c4aa30e
yurisuika <yurisuika@protonmail.com>
2023-07-06 02:14:03 -0700

    whatever

--- src/main/java/dev/yurisuika/raised/mixin/mods/CreateMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/mods/CreateMixin.java
@@ -6,12 +6,10 @@ import com.simibubi.create.content.equipment.toolbox.ToolboxHandlerClient;
 import com.simibubi.create.content.schematics.client.SchematicHotbarSlotOverlay;
 import com.simibubi.create.content.schematics.client.ToolSelectionScreen;
 import dev.yurisuika.raised.Raised;
-import net.minecraft.client.util.Window;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 public class CreateMixin {
 
@@ -30,9 +28,9 @@ public class CreateMixin {
     @Mixin(value = SchematicHotbarSlotOverlay.class, remap = false)
     public static class SchematicHotbarSlotOverlayMixin {
 
-        @Redirect(method = "renderOn", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;getScaledHeight()I"))
-        private int redirectRenderOn(Window instance) {
-            return instance.getScaledHeight() - Raised.getHud();
+        @ModifyVariable(method = "renderOn", at = @At("STORE"), ordinal = 2)
+        private int modifyRenderOn(int value) {
+            return value - Raised.getHud();
         }
 
     }
@@ -52,9 +50,9 @@ public class CreateMixin {
     @Mixin(value = ToolSelectionScreen.class, remap = false)
     public static class ToolSelectionScreenMixin {
 
-        @Redirect(method = "draw", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;getScaledHeight()I"))
-        private int redirectDraw(Window instance) {
-            return instance.getScaledHeight() - Raised.getHud();
+        @ModifyVariable(method = "draw", at = @At("STORE"), ordinal = 1)
+        private int modifyDraw(int value) {
+            return value - Raised.getHud();
         }
 
     }
