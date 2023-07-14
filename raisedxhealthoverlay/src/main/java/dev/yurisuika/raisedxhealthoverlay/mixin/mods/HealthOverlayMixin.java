c0a39158b0f221a9f6ea039b3078178374057d6c
yurisuika <yurisuika@protonmail.com>
2023-07-14 02:45:46 -0700

    Refactored to work with Forge

---
+++ raisedxhealthoverlay/src/main/java/dev/yurisuika/raisedxhealthoverlay/mixin/mods/HealthOverlayMixin.java
@@ -0,0 +1,24 @@
+package dev.yurisuika.raisedxhealthoverlay.mixin.mods;
+
+import dev.yurisuika.raised.Raised;
+import net.minecraft.client.util.Window;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Pseudo;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import terrails.healthoverlay.HealthRenderer;
+
+public class HealthOverlayMixin {
+
+    @Pseudo
+    @Mixin(HealthRenderer.class)
+    public static class HealthRendererMixin {
+
+        @Redirect(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/Window;getScaledHeight()I"))
+        private static int redirectRender(Window instance) {
+            return instance.getScaledHeight() - Raised.getHud();
+        }
+
+    }
+
+}
