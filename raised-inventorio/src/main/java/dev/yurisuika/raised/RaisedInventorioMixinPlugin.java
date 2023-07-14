12aed9590a413139afebe100ad387f0c8349e952
yurisuika <yurisuika@protonmail.com>
2023-07-13 22:26:39 -0700

    Mod support moved into subprojects

--- raised-inventorio/src/main/java/dev/yurisuika/raised/RaisedInventorioMixinPlugin.java
+++ raised-inventorio/src/main/java/dev/yurisuika/raised/RaisedInventorioMixinPlugin.java
@@ -11,27 +11,15 @@ import java.util.Map;
 import java.util.Set;
 import java.util.function.Supplier;
 
-public final class RaisedMixinPlugin implements IMixinConfigPlugin {
-
-    private static final Map<String, Supplier<Boolean>> DETAILARMORBAR = ImmutableMap.of(
-            "dev.yurisuika.raised.mixin.mods.DetailArmorBarMixin$ArmorBarRendererMixin", () -> FMLLoader.getLoadingModList().getModFileById("detailab") != null
-    );
+public final class RaisedInventorioMixinPlugin implements IMixinConfigPlugin {
 
     private static final Map<String, Supplier<Boolean>> INVENTORIO = ImmutableMap.of(
             "dev.yurisuika.raised.mixin.mods.InventorioMixin$HotbarHUDRendererMixin", () -> FMLLoader.getLoadingModList().getModFileById("inventorio") != null
     );
 
-    private static final Map<String, Supplier<Boolean>> INVENTORYPROFILESNEXT = ImmutableMap.of(
-            "dev.yurisuika.raised.mixin.mods.InventoryProfilesNextMixin$LockSlotsHandlerMixin", () -> FMLLoader.getLoadingModList().getModFileById("inventoryprofilesnext")  != null
-    );
-
-    private static final Map<String, Supplier<Boolean>> LEVELHEARTS = ImmutableMap.of(
-            "dev.yurisuika.raised.mixin.mods.LevelHeartsMixin$IngameGuiMixin", () -> FMLLoader.getLoadingModList().getModFileById("levelhearts") != null
-    );
-
     @Override
     public boolean shouldApplyMixin(String targetClassName, String mixinClassName) {
-        return DETAILARMORBAR.getOrDefault(mixinClassName, () -> true).get() && INVENTORIO.getOrDefault(mixinClassName, () -> true).get() && INVENTORYPROFILESNEXT.getOrDefault(mixinClassName, () -> true).get() && LEVELHEARTS.getOrDefault(mixinClassName, () -> true).get();
+        return INVENTORIO.getOrDefault(mixinClassName, () -> true).get();
     }
 
     @Override
