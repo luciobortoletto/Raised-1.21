558b785dbb06079f48010b8bdcf5a29564ae3143
yurisuika <yurisuika@protonmail.com>
2023-09-27 16:55:25 -0700

    more changes

--- src/main/java/dev/yurisuika/raised/client/gui/RaisedGui.java
+++ src/main/java/dev/yurisuika/raised/client/gui/RaisedGui.java
@@ -2,7 +2,6 @@ package dev.yurisuika.raised.client.gui;
 
 import com.google.common.collect.Lists;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.util.Identifier;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
 import net.minecraftforge.client.gui.overlay.NamedGuiOverlay;
@@ -16,9 +15,7 @@ import static net.minecraftforge.client.gui.overlay.VanillaGuiOverlay.*;
 
 public class RaisedGui extends ForgeGui {
 
-    private static final Identifier WIDGETS = new Identifier("textures/gui/widgets.png");
-    
-    private final List<NamedGuiOverlay> hud = Lists.newArrayList(
+    public final List<NamedGuiOverlay> hud = Lists.newArrayList(
             HOTBAR.type(),
             PLAYER_HEALTH.type(),
             ARMOR_LEVEL.type(),
@@ -27,20 +24,13 @@ public class RaisedGui extends ForgeGui {
             MOUNT_HEALTH.type(),
             JUMP_BAR.type(),
             EXPERIENCE_BAR.type(),
-            EXPERIENCE_BAR.type(),
             ITEM_NAME.type(),
             RECORD_OVERLAY.type()
     );
-
-    private final List<NamedGuiOverlay> chat = Lists.newArrayList(
+    public final List<NamedGuiOverlay> chat = Lists.newArrayList(
             CHAT_PANEL.type()
     );
-
-    private final List<String> mods = Lists.newArrayList(
-            "colorfulhearts:health"
-    );
-
-    private final List<NamedGuiOverlay> other = Lists.newArrayList(
+    public final List<NamedGuiOverlay> all = Lists.newArrayList(
             VIGNETTE.type(),
             SPYGLASS.type(),
             HELMET.type(),
@@ -73,62 +63,75 @@ public class RaisedGui extends ForgeGui {
         super(MinecraftClient.getInstance());
     }
 
-    // START TRANSLATION OF EVENTS
+    // HUD
+    // START TRANSLATION OF HUD EVENTS
     @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
-    public void translatePreHighest(RenderGuiOverlayEvent.Pre event) {
+    public void startHudTranslate(RenderGuiOverlayEvent.Pre event) {
+        if (hud.contains(event.getOverlay())) {
+            event.getGuiGraphics().getMatrices().translate(0, -getHud(), 0);
+        }
+    }
+    // END TRANSLATION OF HUD EVENTS (CANCELLED)
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    public void endHudTranslate(RenderGuiOverlayEvent.Pre event) {
+        if (hud.contains(event.getOverlay()) && event.isCanceled()) {
+            event.getGuiGraphics().getMatrices().translate(0, +getHud(), 0);
+        }
+    }
+    // END TRANSLATION OF HUD EVENTS
+    @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
+    public void endHudTranslate(RenderGuiOverlayEvent.Post event) {
+        if (hud.contains(event.getOverlay())) {
+            event.getGuiGraphics().getMatrices().translate(0, +getHud(), 0);
+        }
+    }
 
-        if (!other.contains(event.getOverlay()) && getSupport().pre) {
-            event.getGuiGraphics().getMatrices().translate(0, -getHud(), 0);
-        } else if (hud.contains(event.getOverlay())) {
-            event.getGuiGraphics().getMatrices().translate(0, -getHud(), 0);
-        } else if (chat.contains(event.getOverlay())) {
+    // CHAT
+    // START TRANSLATION OF CHAT EVENTS
+    @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
+    public void startChatTranslate(RenderGuiOverlayEvent.Pre event) {
+        if (chat.contains(event.getOverlay())) {
             event.getGuiGraphics().getMatrices().translate(0, -getChat(), +300);
         }
-
-        if (mods.contains(event.getOverlay().id().toString())) {
-//            event.getGuiGraphics().getMatrices().translate(0, -getChat(), +300);
-        }
-
     }
-
-    // START TRANSLATION OF EVENTS
-    @SubscribeEvent(priority = EventPriority.HIGHEST)
-    public void translatePostHighest(RenderGuiOverlayEvent.Post event) {
-
-        if (!other.contains(event.getOverlay()) && getSupport().post) {
-            event.getGuiGraphics().getMatrices().translate(0, -getHud(), 0);
-        }
-
-    }
-
-    // END TRANSLATION OF EVENTS
+    // END TRANSLATION OF CHAT EVENTS
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
-    public void translatePreLowest(RenderGuiOverlayEvent.Pre event) {
-
-        if (!other.contains(event.getOverlay()) && getSupport().pre) {
-            event.getGuiGraphics().getMatrices().translate(0, +getHud(), 0);
-        } else if (hud.contains(event.getOverlay()) && event.isCanceled()) {
-            event.getGuiGraphics().getMatrices().translate(0, +getHud(), 0);
-        }
-
-    }
-
-    // END TRANSLATION OF EVENTS
-    @SubscribeEvent(priority = EventPriority.LOWEST)
-    public void translatePostLowest(RenderGuiOverlayEvent.Post event) {
-
-        if (!other.contains(event.getOverlay()) && getSupport().post) {
-            event.getGuiGraphics().getMatrices().translate(0, +getHud(), 0);
-        } else if (hud.contains(event.getOverlay())) {
-            event.getGuiGraphics().getMatrices().translate(0, +getHud(), 0);
-        } else if (chat.contains(event.getOverlay())) {
+    public void endChatTranslate(RenderGuiOverlayEvent.Pre event) {
+        if (chat.contains(event.getOverlay())) {
             event.getGuiGraphics().getMatrices().translate(0, +getChat(), -300);
         }
+    }
 
-        if (mods.contains(event.getOverlay().id().toString())) {
-//            event.getGuiGraphics().getMatrices().translate(0, +getChat(), +300);
+    // PRE MOD
+    // START TRANSLATION OF PRE MOD EVENTS
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void startPreModTranslate(RenderGuiOverlayEvent.Pre event) {
+        if (!all.contains(event.getOverlay()) && getSupport().pre) {
+            event.getGuiGraphics().getMatrices().translate(0, -getHud(), 0);
         }
+    }
+    // END TRANSLATION OF PRE MOD EVENTS
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void endPreModTranslate(RenderGuiOverlayEvent.Pre event) {
+        if (!all.contains(event.getOverlay()) && getSupport().pre) {
+            event.getGuiGraphics().getMatrices().translate(0, +getHud(), 0);
+        }
+    }
 
+    // POST MOD
+    // START TRANSLATION OF POST MOD EVENTS
+    @SubscribeEvent(priority = EventPriority.HIGHEST)
+    public void startPostModTranslate(RenderGuiOverlayEvent.Post event) {
+        if (!all.contains(event.getOverlay()) && getSupport().post) {
+            event.getGuiGraphics().getMatrices().translate(0, -getHud(), 0);
+        }
+    }
+    // END TRANSLATION OF POST MOD EVENTS
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void endPostModTranslate(RenderGuiOverlayEvent.Post event) {
+        if (!all.contains(event.getOverlay()) && getSupport().post) {
+            event.getGuiGraphics().getMatrices().translate(0, +getHud(), 0);
+        }
     }
 
 }
--- src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
@@ -2,31 +2,21 @@ package dev.yurisuika.raised.mixin.client.gui.hud;
 
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import static dev.yurisuika.raised.client.option.RaisedConfig.*;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(value = InGameHud.class, priority = -999999999)
 public abstract class InGameHudMixin {
 
-    // HELD ITEM TOOLTIP
-    @Inject(method = "renderHeldItemTooltip", at = @At("HEAD"))
-    private void heldItemTooltipStart(DrawContext context, CallbackInfo ci) {
-        context.getMatrices().translate(0, -getHud(), 0);
-    }
-    @Inject(method = "renderHeldItemTooltip", at = @At("TAIL"))
-    private void heldItemTooltipEnd(DrawContext context, CallbackInfo ci) {
-        context.getMatrices().translate(0, +getHud(), 0);
-    }
-
     // HOTBAR SELECTOR
-    @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 1), index = 6)
-    private int resizeHotbarSelector(int value) {
-        return 24;
+    @Inject(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/math/MatrixStack;pop()V"), locals = LocalCapture.CAPTURE_FAILHARD)
+    private void drawHotbarSelector(float tickDelta, DrawContext context, CallbackInfo ci, PlayerEntity playerEntity) {
+        context.drawTexture(new Identifier("textures/gui/sprites/hud/hotbar_selection.png"), (context.getScaledWindowWidth() / 2) - 92 + playerEntity.getInventory().selectedSlot * 20, context.getScaledWindowHeight(), 0, 0, 24, 1);
     }
 
 }
