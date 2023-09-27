1ea000180390041f4388fd1419df54333c219e01
yurisuika <yurisuika@protonmail.com>
2023-09-27 16:54:20 -0700

    mod rewrite

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
@@ -1,27 +1,12 @@
 package dev.yurisuika.raised.mixin.client.gui.hud;
 
-import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
-import static dev.yurisuika.raised.client.option.RaisedConfig.*;
 
 @Mixin(value = InGameHud.class, priority = -999999999)
 public abstract class InGameHudMixin {
-
-    // HELD ITEM TOOLTIP
-    @Inject(method = "renderHeldItemTooltip", at = @At("HEAD"))
-    private void heldItemTooltipStart(DrawContext context, CallbackInfo ci) {
-        context.getMatrices().translate(0, -getHud(), 0);
-    }
-    @Inject(method = "renderHeldItemTooltip", at = @At("TAIL"))
-    private void heldItemTooltipEnd(DrawContext context, CallbackInfo ci) {
-        context.getMatrices().translate(0, +getHud(), 0);
-    }
 
     // HOTBAR SELECTOR
     @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lnet/minecraft/util/Identifier;IIIIII)V", ordinal = 1), index = 6)
