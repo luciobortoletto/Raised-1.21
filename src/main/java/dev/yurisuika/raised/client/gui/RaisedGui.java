00d270d85ddc97ce2ec0e63d894adb671088c07d
yurisuika <yurisuika@protonmail.com>
2023-10-04 07:46:04 -0700

    RegisterGuiOverlaysEvent

--- src/main/java/dev/yurisuika/raised/client/gui/RaisedGui.java
+++ src/main/java/dev/yurisuika/raised/client/gui/RaisedGui.java
@@ -2,9 +2,9 @@ package dev.yurisuika.raised.client.gui;
 
 import com.google.common.collect.Lists;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.util.Identifier;
 import net.minecraftforge.client.event.RenderGuiOverlayEvent;
 import net.minecraftforge.client.gui.overlay.ForgeGui;
-import net.minecraftforge.client.gui.overlay.NamedGuiOverlay;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -15,48 +15,48 @@ import static net.minecraftforge.client.gui.overlay.VanillaGuiOverlay.*;
 
 public class RaisedGui extends ForgeGui {
 
-    public final List<NamedGuiOverlay> hud = Lists.newArrayList(
-            HOTBAR.type(),
-            PLAYER_HEALTH.type(),
-            ARMOR_LEVEL.type(),
-            FOOD_LEVEL.type(),
-            AIR_LEVEL.type(),
-            MOUNT_HEALTH.type(),
-            JUMP_BAR.type(),
-            EXPERIENCE_BAR.type(),
-            ITEM_NAME.type(),
-            RECORD_OVERLAY.type()
+    public static List<Identifier> hud = Lists.newArrayList(
+            HOTBAR.id(),
+            PLAYER_HEALTH.id(),
+            ARMOR_LEVEL.id(),
+            FOOD_LEVEL.id(),
+            AIR_LEVEL.id(),
+            MOUNT_HEALTH.id(),
+            JUMP_BAR.id(),
+            EXPERIENCE_BAR.id(),
+            ITEM_NAME.id(),
+            RECORD_OVERLAY.id()
     );
-    public final List<NamedGuiOverlay> chat = Lists.newArrayList(
-            CHAT_PANEL.type()
+    public static List<Identifier> chat = Lists.newArrayList(
+            CHAT_PANEL.id()
     );
-    public final List<NamedGuiOverlay> all = Lists.newArrayList(
-            VIGNETTE.type(),
-            SPYGLASS.type(),
-            HELMET.type(),
-            FROSTBITE.type(),
-            PORTAL.type(),
-            HOTBAR.type(),
-            CROSSHAIR.type(),
-            BOSS_EVENT_PROGRESS.type(),
-            PLAYER_HEALTH.type(),
-            ARMOR_LEVEL.type(),
-            FOOD_LEVEL.type(),
-            AIR_LEVEL.type(),
-            MOUNT_HEALTH.type(),
-            JUMP_BAR.type(),
-            EXPERIENCE_BAR.type(),
-            ITEM_NAME.type(),
-            SLEEP_FADE.type(),
-            POTION_ICONS.type(),
-            DEBUG_TEXT.type(),
-            FPS_GRAPH.type(),
-            RECORD_OVERLAY.type(),
-            TITLE_TEXT.type(),
-            SUBTITLES.type(),
-            SCOREBOARD.type(),
-            CHAT_PANEL.type(),
-            PLAYER_LIST.type()
+    public static List<Identifier> all = Lists.newArrayList(
+            VIGNETTE.id(),
+            SPYGLASS.id(),
+            HELMET.id(),
+            FROSTBITE.id(),
+            PORTAL.id(),
+            HOTBAR.id(),
+            CROSSHAIR.id(),
+            BOSS_EVENT_PROGRESS.id(),
+            PLAYER_HEALTH.id(),
+            ARMOR_LEVEL.id(),
+            FOOD_LEVEL.id(),
+            AIR_LEVEL.id(),
+            MOUNT_HEALTH.id(),
+            JUMP_BAR.id(),
+            EXPERIENCE_BAR.id(),
+            ITEM_NAME.id(),
+            SLEEP_FADE.id(),
+            POTION_ICONS.id(),
+            DEBUG_TEXT.id(),
+            FPS_GRAPH.id(),
+            RECORD_OVERLAY.id(),
+            TITLE_TEXT.id(),
+            SUBTITLES.id(),
+            SCOREBOARD.id(),
+            CHAT_PANEL.id(),
+            PLAYER_LIST.id()
     );
 
     public RaisedGui() {
@@ -67,21 +67,21 @@ public class RaisedGui extends ForgeGui {
     // START TRANSLATION OF HUD EVENTS
     @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
     public void startHudTranslate(RenderGuiOverlayEvent.Pre event) {
-        if (hud.contains(event.getOverlay())) {
+        if (hud.contains(event.getOverlay().id())) {
             event.getPoseStack().translate(0, -getHud(), 0);
         }
     }
     // END TRANSLATION OF HUD EVENTS (CANCELLED)
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
     public void endHudTranslate(RenderGuiOverlayEvent.Pre event) {
-        if (hud.contains(event.getOverlay()) && event.isCanceled()) {
+        if (hud.contains(event.getOverlay().id()) && event.isCanceled()) {
             event.getPoseStack().translate(0, +getHud(), 0);
         }
     }
     // END TRANSLATION OF HUD EVENTS
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
     public void endHudTranslate(RenderGuiOverlayEvent.Post event) {
-        if (hud.contains(event.getOverlay())) {
+        if (hud.contains(event.getOverlay().id())) {
             event.getPoseStack().translate(0, +getHud(), 0);
         }
     }
@@ -90,14 +90,14 @@ public class RaisedGui extends ForgeGui {
     // START TRANSLATION OF CHAT EVENTS
     @SubscribeEvent(priority = EventPriority.HIGHEST, receiveCanceled = true)
     public void startChatTranslate(RenderGuiOverlayEvent.Pre event) {
-        if (chat.contains(event.getOverlay())) {
+        if (chat.contains(event.getOverlay().id())) {
             event.getPoseStack().translate(0, -getChat(), +300);
         }
     }
     // END TRANSLATION OF CHAT EVENTS
     @SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
-    public void endChatTranslate(RenderGuiOverlayEvent.Pre event) {
-        if (chat.contains(event.getOverlay())) {
+    public void endChatTranslate(RenderGuiOverlayEvent.Post event) {
+        if (chat.contains(event.getOverlay().id())) {
             event.getPoseStack().translate(0, +getChat(), -300);
         }
     }
@@ -106,14 +106,14 @@ public class RaisedGui extends ForgeGui {
     // START TRANSLATION OF PRE MOD EVENTS
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void startPreModTranslate(RenderGuiOverlayEvent.Pre event) {
-        if (!all.contains(event.getOverlay()) && getSupport().pre) {
+        if (!all.contains(event.getOverlay().id()) && getSupport().pre) {
             event.getPoseStack().translate(0, -getHud(), 0);
         }
     }
     // END TRANSLATION OF PRE MOD EVENTS
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void endPreModTranslate(RenderGuiOverlayEvent.Pre event) {
-        if (!all.contains(event.getOverlay()) && getSupport().pre) {
+        if (!all.contains(event.getOverlay().id()) && getSupport().pre) {
             event.getPoseStack().translate(0, +getHud(), 0);
         }
     }
@@ -122,14 +122,14 @@ public class RaisedGui extends ForgeGui {
     // START TRANSLATION OF POST MOD EVENTS
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void startPostModTranslate(RenderGuiOverlayEvent.Post event) {
-        if (!all.contains(event.getOverlay()) && getSupport().post) {
+        if (!all.contains(event.getOverlay().id()) && getSupport().post) {
             event.getPoseStack().translate(0, -getHud(), 0);
         }
     }
     // END TRANSLATION OF POST MOD EVENTS
     @SubscribeEvent(priority = EventPriority.LOWEST)
     public void endPostModTranslate(RenderGuiOverlayEvent.Post event) {
-        if (!all.contains(event.getOverlay()) && getSupport().post) {
+        if (!all.contains(event.getOverlay().id()) && getSupport().post) {
             event.getPoseStack().translate(0, +getHud(), 0);
         }
     }
