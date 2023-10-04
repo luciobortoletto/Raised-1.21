5cd13cfce64abedeec814ab7b369a897d431ac43
yurisuika <yurisuika@protonmail.com>
2023-10-04 07:46:26 -0700

    RegisterGuiOverlaysEvent

---
+++ src/main/java/dev/yurisuika/raised/mixin/client/event/RegisterGuiOverlaysEventMixin.java
@@ -0,0 +1,35 @@
+package dev.yurisuika.raised.mixin.client.event;
+
+import net.minecraft.util.Identifier;
+import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
+import net.minecraftforge.client.gui.overlay.IGuiOverlay;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static dev.yurisuika.raised.client.gui.RaisedGui.*;
+
+@Mixin(RegisterGuiOverlaysEvent.class)
+public class RegisterGuiOverlaysEventMixin {
+
+    @Inject(method = "registerBelow", at = @At("HEAD"))
+    private void translateOverlayBelow(Identifier other, String id, IGuiOverlay overlay, CallbackInfo ci) {
+        if (hud.contains(other)) {
+            FMLLoader.getLoadingModList().getMods().forEach(mod -> {
+                hud.add(Identifier.tryParse(mod.getNamespace() + ":" + id));
+            });
+        }
+    }
+
+    @Inject(method = "registerAbove", at = @At("HEAD"))
+    private void translateOverlayAbove(Identifier other, String id, IGuiOverlay overlay, CallbackInfo ci) {
+        if (hud.contains(other)) {
+            FMLLoader.getLoadingModList().getMods().forEach(mod -> {
+                hud.add(Identifier.tryParse(mod.getNamespace() + ":" + id));
+            });
+        }
+    }
+
+}
