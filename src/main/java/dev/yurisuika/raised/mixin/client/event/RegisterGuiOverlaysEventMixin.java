632efd9337143a0e56127a9bbbe4d575bc0db132
yurisuika <yurisuika@protonmail.com>
2023-10-04 21:52:17 -0700

    Simplify extended support

--- src/main/java/dev/yurisuika/raised/mixin/client/event/RegisterGuiOverlaysEventMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/event/RegisterGuiOverlaysEventMixin.java
@@ -10,22 +10,25 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import static dev.yurisuika.raised.client.gui.RaisedGui.*;
+import static dev.yurisuika.raised.client.option.RaisedConfig.*;
 
 @Mixin(RegisterGuiOverlaysEvent.class)
 public class RegisterGuiOverlaysEventMixin {
 
+    // PRE MOD
     @Inject(method = "registerBelow", at = @At("HEAD"))
     private void translateOverlayBelow(Identifier other, String id, IGuiOverlay overlay, CallbackInfo ci) {
-        if (hud.contains(other)) {
+        if (hud.contains(other) && getSupport()) {
             FMLLoader.getLoadingModList().getMods().forEach(mod -> {
                 hud.add(Identifier.tryParse(mod.getNamespace() + ":" + id));
             });
         }
     }
 
+    // POST MOD
     @Inject(method = "registerAbove", at = @At("HEAD"))
     private void translateOverlayAbove(Identifier other, String id, IGuiOverlay overlay, CallbackInfo ci) {
-        if (hud.contains(other)) {
+        if (hud.contains(other) && getSupport()) {
             FMLLoader.getLoadingModList().getMods().forEach(mod -> {
                 hud.add(Identifier.tryParse(mod.getNamespace() + ":" + id));
             });
