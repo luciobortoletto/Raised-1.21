dcc4f66b154f64baf879671e78011be9908609b4
yurisuika <yurisuika@protonmail.com>
2023-10-18 08:27:04 -0700

    test to see if this is the cause of performance loss

--- src/main/java/dev/yurisuika/raised/mixin/client/event/RegisterGuiOverlaysEventMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/event/RegisterGuiOverlaysEventMixin.java
@@ -4,6 +4,7 @@ import net.minecraft.util.Identifier;
 import net.minecraftforge.client.event.RegisterGuiOverlaysEvent;
 import net.minecraftforge.client.gui.overlay.IGuiOverlay;
 import net.minecraftforge.fml.loading.FMLLoader;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -18,21 +19,23 @@ public class RegisterGuiOverlaysEventMixin {
     // PRE MOD
     @Inject(method = "registerBelow", at = @At("HEAD"))
     private void addOverlayBelow(Identifier other, String id, IGuiOverlay overlay, CallbackInfo ci) {
-        if (hud.contains(other) && getSupport()) {
-            FMLLoader.getLoadingModList().getMods().forEach(mod -> {
-                hud.add(Identifier.tryParse(mod.getNamespace() + ":" + id));
-            });
-        }
+//        if (hud.contains(other) && getSupport()) {
+//            FMLLoader.getLoadingModList().getMods().forEach(mod -> {
+//                hud.add(Identifier.tryParse(mod.getNamespace() + ":" + id));
+//                LoggerFactory.getLogger("raised").info("Registering pre mod element: " + mod.getNamespace() + ":" + id);
+//            });
+//        }
     }
 
     // POST MOD
     @Inject(method = "registerAbove", at = @At("HEAD"))
     private void addOverlayAbove(Identifier other, String id, IGuiOverlay overlay, CallbackInfo ci) {
-        if (hud.contains(other) && getSupport()) {
-            FMLLoader.getLoadingModList().getMods().forEach(mod -> {
-                hud.add(Identifier.tryParse(mod.getNamespace() + ":" + id));
-            });
-        }
+//        if (hud.contains(other) && getSupport()) {
+//            FMLLoader.getLoadingModList().getMods().forEach(mod -> {
+//                hud.add(Identifier.tryParse(mod.getNamespace() + ":" + id));
+//                LoggerFactory.getLogger("raised").info("Registering post mod element: " + mod.getNamespace() + ":" + id);
+//            });
+//        }
     }
 
 }
