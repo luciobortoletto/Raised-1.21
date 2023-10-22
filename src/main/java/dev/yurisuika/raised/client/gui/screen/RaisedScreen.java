52dfe28b9f4bc62bd50cc757ff1ae94dd81db259
yurisuika <yurisuika@protonmail.com>
2023-10-22 16:41:38 -0700

    replaced keybind movement with screen

---
+++ src/main/java/dev/yurisuika/raised/client/gui/screen/RaisedScreen.java
@@ -0,0 +1,46 @@
+package dev.yurisuika.raised.client.gui.screen;
+
+import dev.yurisuika.raised.client.option.RaisedConfig;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.option.GameOptions;
+import net.minecraft.client.option.SimpleOption;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.screen.ScreenTexts;
+import net.minecraft.text.Text;
+
+import static dev.yurisuika.raised.client.option.RaisedConfig.*;
+
+@Environment(EnvType.CLIENT)
+public class RaisedScreen extends Screen {
+
+    public RaisedScreen(Text title) {
+        super(title);
+    }
+
+    @Override
+    public void init() {
+        addDrawableChild(new SimpleOption<>("options.raised.hud", SimpleOption.constantTooltip(Text.translatable("options.raised.hud.tooltip")), (prefix, value) -> value == 0 ? GameOptions.getGenericValueText(prefix, ScreenTexts.OFF) : GameOptions.getGenericValueText(prefix, Text.literal(String.valueOf(value))), new SimpleOption.ValidatingIntSliderCallbacks(0, client.getWindow().getScaledHeight() / 2), getHud(), RaisedConfig::setHud).createButton(client.options, 16, 32, 200));
+        addDrawableChild(new SimpleOption<>("options.raised.chat", SimpleOption.constantTooltip(Text.translatable("options.raised.chat.tooltip")), (prefix, value) -> value == 0 ? GameOptions.getGenericValueText(prefix, ScreenTexts.OFF) : GameOptions.getGenericValueText(prefix, Text.literal(String.valueOf(value))), new SimpleOption.ValidatingIntSliderCallbacks(0, client.getWindow().getScaledHeight() / 2), getChat(), RaisedConfig::setChat).createButton(client.options, 16, 56, 200));
+        addDrawableChild(SimpleOption.ofBoolean("options.raised.support", SimpleOption.constantTooltip(Text.translatable("options.raised.support.tooltip")), getSupport(), RaisedConfig::setSupport).createButton(client.options, 16, 80, 98));
+        addDrawableChild(SimpleOption.ofBoolean("options.raised.sync", SimpleOption.constantTooltip(Text.translatable("options.raised.sync.tooltip")), getSync(), RaisedConfig::setSync).createButton(client.options, 118, 80, 98));
+        addDrawableChild(SimpleOption.ofBoolean("options.raised.share", SimpleOption.constantTooltip(Text.translatable("options.raised.share.tooltip")), getShare(), RaisedConfig::setShare).createButton(client.options, 16, 104, 200));
+    }
+
+    @Override
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        super.render(matrices, mouseX, mouseY, delta);
+        drawTextWithShadow(matrices, this.textRenderer, Text.translatable("options.raised.title"), 16, 16, -1);
+    }
+
+    @Override
+    public void renderBackground(MatrixStack matrices) {
+    }
+
+    @Override
+    public boolean shouldPause() {
+        return false;
+    }
+
+}
