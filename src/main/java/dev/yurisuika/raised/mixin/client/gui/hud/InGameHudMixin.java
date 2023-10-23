6c3ea05956b30280584279fdbbc7fe15373eeb5b
yurisuika <yurisuika@protonmail.com>
2023-10-23 01:07:47 -0700

    forge doesn't support modifyargs

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/hud/InGameHudMixin.java
@@ -4,17 +4,20 @@ import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(value = InGameHud.class, priority = -999999999)
 public abstract class InGameHudMixin {
 
     // HOTBAR SELECTOR
-    @ModifyArgs(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1))
-    private void resizeHotbarSelector(Args args) {
-        args.set(0, new Identifier("raised:hud/hotbar_selection"));
-        args.set(4, 24);
+    @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1), index = 0)
+    private Identifier changeHotbarSelectorNamespace(Identifier texture) {
+        return new Identifier("raised:hud/hotbar");
+    }
+
+    @ModifyArg(method = "renderHotbar", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawGuiTexture(Lnet/minecraft/util/Identifier;IIII)V", ordinal = 1), index = 4)
+    private int resizeHotbarSelector(int value) {
+        return 24;
     }
 
 }
