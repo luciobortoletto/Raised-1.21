73981caa4259940bd42d18b89b3a4aac27ad7138
yurisuika <yurisuika@protonmail.com>
2023-10-23 11:18:06 -0700

    optional if present & added slider tooltips

--- src/main/java/dev/yurisuika/raised/client/gui/screen/RaisedScreen.java
+++ src/main/java/dev/yurisuika/raised/client/gui/screen/RaisedScreen.java
@@ -3,6 +3,7 @@ package dev.yurisuika.raised.client.gui.screen;
 import dev.yurisuika.raised.mixin.client.option.OptionInvoker;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ScreenTexts;
 import net.minecraft.client.gui.widget.ClickableWidget;
@@ -29,8 +30,14 @@ public class RaisedScreen extends Screen {
 
     @Override
     public void init() {
-        grid.add(new DoubleOption("options.raised.hud", 0, client.getWindow().getScaledHeight() / 2, 1.0F, gameOptions -> (double)getHud(), (gameOptions, value) -> setHud(value.intValue()), (gameOptions, option) -> option.get(gameOptions) == 0 ? ((OptionInvoker)option).invokeGetGenericLabel(ScreenTexts.OFF) : ((OptionInvoker)option).invokeGetGenericLabel(new LiteralText(String.valueOf((int)option.get(gameOptions))))).createButton(client.options, 16, 32, 200));
-        grid.add(new DoubleOption("options.raised.chat", 0, client.getWindow().getScaledHeight() / 2, 1.0F, gameOptions -> (double)getChat(), (gameOptions, value) -> setChat(value.intValue()), (gameOptions, option) -> option.get(gameOptions) == 0 ? ((OptionInvoker)option).invokeGetGenericLabel(ScreenTexts.OFF) : ((OptionInvoker)option).invokeGetGenericLabel(new LiteralText(String.valueOf((int)option.get(gameOptions))))).createButton(client.options, 16, 56, 200));
+        grid.add(new DoubleOption("options.raised.hud", 0, client.getWindow().getScaledHeight() / 2, 1.0F, gameOptions -> (double)getHud(), (gameOptions, value) -> setHud(value.intValue()), (gameOptions, option) -> {
+            option.setTooltip(MinecraftClient.getInstance().textRenderer.wrapLines(new TranslatableText("options.raised.hud.tooltip"), 200));
+            return option.get(gameOptions) == 0 ? ((OptionInvoker)option).invokeGetGenericLabel(ScreenTexts.OFF) : ((OptionInvoker)option).invokeGetGenericLabel(new LiteralText(String.valueOf((int)option.get(gameOptions))));
+        }).createButton(client.options, 16, 32, 200));
+        grid.add(new DoubleOption("options.raised.chat", 0, client.getWindow().getScaledHeight() / 2, 1.0F, gameOptions -> (double)getChat(), (gameOptions, value) -> setChat(value.intValue()), (gameOptions, option) -> {
+            option.setTooltip(MinecraftClient.getInstance().textRenderer.wrapLines(new TranslatableText("options.raised.chat.tooltip"), 200));
+            return option.get(gameOptions) == 0 ? ((OptionInvoker)option).invokeGetGenericLabel(ScreenTexts.OFF) : ((OptionInvoker)option).invokeGetGenericLabel(new LiteralText(String.valueOf((int)option.get(gameOptions))));
+        }).createButton(client.options, 16, 56, 200));
         grid.add(new BooleanOption("options.raised.support", new TranslatableText("options.raised.support.tooltip"), gameOptions -> getSupport(), (gameOptions, value) -> setSupport(value)).createButton(client.options, 16, 80, 98));
         grid.add(new BooleanOption("options.raised.sync", new TranslatableText("options.raised.sync.tooltip"), gameOptions -> getSync(), (gameOptions, value) -> setSync(value)).createButton(client.options, 118, 80, 98));
         grid.add(new BooleanOption("options.raised.share", new TranslatableText("options.raised.share.tooltip"), gameOptions -> getShare(), (gameOptions, value) -> setShare(value)).createButton(client.options, 16, 104, 200));
@@ -44,8 +51,8 @@ public class RaisedScreen extends Screen {
         super.render(matrices, mouseX, mouseY, delta);
         drawTextWithShadow(matrices, this.textRenderer, new TranslatableText("options.raised.title"), 16, 16, -1);
         for (ClickableWidget option : grid) {
-            if (option != null && option.isMouseOver(mouseX, mouseY)) {
-                renderOrderedTooltip(matrices, ((OrderableTooltip)option).getOrderedTooltip().get(), mouseX, mouseY);
+            if (option != null && option.isMouseOver(mouseX, mouseY) && ((OrderableTooltip)option).getOrderedTooltip().isPresent()) {
+                renderOrderedTooltip(matrices, ((OrderableTooltip)option).getOrderedTooltip().orElse(null), mouseX, mouseY);
             }
         }
     }
