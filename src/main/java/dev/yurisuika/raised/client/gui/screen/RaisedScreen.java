7eae2c9ea3ed03b67bd44aa891a3a579574fde3c
yurisuika <yurisuika@protonmail.com>
2023-10-23 09:20:42 -0700

    getting tooltips to work

--- src/main/java/dev/yurisuika/raised/client/gui/screen/RaisedScreen.java
+++ src/main/java/dev/yurisuika/raised/client/gui/screen/RaisedScreen.java
@@ -4,16 +4,22 @@ import dev.yurisuika.raised.client.option.RaisedConfig;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.client.option.SimpleOption;
+import net.minecraft.client.util.OrderableTooltip;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.ScreenTexts;
 import net.minecraft.text.Text;
+
+import java.util.ArrayList;
 
 import static dev.yurisuika.raised.client.option.RaisedConfig.*;
 
 @Environment(EnvType.CLIENT)
 public class RaisedScreen extends Screen {
+
+    ArrayList<ClickableWidget> grid = new ArrayList<>();
 
     public RaisedScreen(Text title) {
         super(title);
@@ -21,17 +27,25 @@ public class RaisedScreen extends Screen {
 
     @Override
     public void init() {
-        addDrawableChild(new SimpleOption<>("options.raised.hud", SimpleOption.constantTooltip(Text.translatable("options.raised.hud.tooltip")), (prefix, value) -> value == 0 ? GameOptions.getGenericValueText(prefix, ScreenTexts.OFF) : GameOptions.getGenericValueText(prefix, Text.literal(String.valueOf(value))), new SimpleOption.ValidatingIntSliderCallbacks(0, client.getWindow().getScaledHeight() / 2), getHud(), RaisedConfig::setHud).createButton(client.options, 16, 32, 200));
-        addDrawableChild(new SimpleOption<>("options.raised.chat", SimpleOption.constantTooltip(Text.translatable("options.raised.chat.tooltip")), (prefix, value) -> value == 0 ? GameOptions.getGenericValueText(prefix, ScreenTexts.OFF) : GameOptions.getGenericValueText(prefix, Text.literal(String.valueOf(value))), new SimpleOption.ValidatingIntSliderCallbacks(0, client.getWindow().getScaledHeight() / 2), getChat(), RaisedConfig::setChat).createButton(client.options, 16, 56, 200));
-        addDrawableChild(SimpleOption.ofBoolean("options.raised.support", SimpleOption.constantTooltip(Text.translatable("options.raised.support.tooltip")), getSupport(), RaisedConfig::setSupport).createButton(client.options, 16, 80, 98));
-        addDrawableChild(SimpleOption.ofBoolean("options.raised.sync", SimpleOption.constantTooltip(Text.translatable("options.raised.sync.tooltip")), getSync(), RaisedConfig::setSync).createButton(client.options, 118, 80, 98));
-        addDrawableChild(SimpleOption.ofBoolean("options.raised.share", SimpleOption.constantTooltip(Text.translatable("options.raised.share.tooltip")), getShare(), RaisedConfig::setShare).createButton(client.options, 16, 104, 200));
+        grid.add(new SimpleOption<>("options.raised.hud", SimpleOption.constantTooltip(Text.translatable("options.raised.hud.tooltip")), (prefix, value) -> value == 0 ? GameOptions.getGenericValueText(prefix, ScreenTexts.OFF) : GameOptions.getGenericValueText(prefix, Text.literal(String.valueOf(value))), new SimpleOption.ValidatingIntSliderCallbacks(0, client.getWindow().getScaledHeight() / 2), getHud(), RaisedConfig::setHud).createButton(client.options, 16, 32, 200));
+        grid.add(new SimpleOption<>("options.raised.chat", SimpleOption.constantTooltip(Text.translatable("options.raised.chat.tooltip")), (prefix, value) -> value == 0 ? GameOptions.getGenericValueText(prefix, ScreenTexts.OFF) : GameOptions.getGenericValueText(prefix, Text.literal(String.valueOf(value))), new SimpleOption.ValidatingIntSliderCallbacks(0, client.getWindow().getScaledHeight() / 2), getChat(), RaisedConfig::setChat).createButton(client.options, 16, 56, 200));
+        grid.add(SimpleOption.ofBoolean("options.raised.support", SimpleOption.constantTooltip(Text.translatable("options.raised.support.tooltip")), getSupport(), RaisedConfig::setSupport).createButton(client.options, 16, 80, 98));
+        grid.add(SimpleOption.ofBoolean("options.raised.sync", SimpleOption.constantTooltip(Text.translatable("options.raised.sync.tooltip")), getSync(), RaisedConfig::setSync).createButton(client.options, 118, 80, 98));
+        grid.add(SimpleOption.ofBoolean("options.raised.share", SimpleOption.constantTooltip(Text.translatable("options.raised.share.tooltip")), getShare(), RaisedConfig::setShare).createButton(client.options, 16, 104, 200));
+        for (ClickableWidget option : grid) {
+            addDrawableChild(option);
+        }
     }
 
     @Override
     public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
         super.render(matrices, mouseX, mouseY, delta);
         drawTextWithShadow(matrices, this.textRenderer, Text.translatable("options.raised.title"), 16, 16, -1);
+        for (ClickableWidget option : grid) {
+            if (option != null && option.isMouseOver(mouseX, mouseY)) {
+                renderOrderedTooltip(matrices, ((OrderableTooltip)option).getOrderedTooltip(), mouseX, mouseY);
+            }
+        }
     }
 
     @Override
