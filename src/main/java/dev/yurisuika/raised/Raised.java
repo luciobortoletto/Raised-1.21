2d635a88d7d7fbf11a0c26a646a707dd1c8b620a
yurisuika <yurisuika@protonmail.com>
2024-02-24 05:08:36 -0800

    text options screen

--- src/main/java/dev/yurisuika/raised/Raised.java
+++ src/main/java/dev/yurisuika/raised/Raised.java
@@ -18,7 +18,7 @@ public class Raised implements ClientModInitializer {
     public static void registerClientTickEvents() {
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (options.wasPressed()) {
-                client.openScreen(new RaisedScreen(new TranslatableText("options.raised.title")));
+                client.openScreen(new RaisedScreen.SliderScreen(new TranslatableText("options.raised.title")));
             }
         });
     }
