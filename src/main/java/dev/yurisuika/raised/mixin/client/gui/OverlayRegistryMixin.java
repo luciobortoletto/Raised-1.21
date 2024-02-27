1026ca5e34fb7b30288a67e9fbc035291815ef10
yurisuika <yurisuika@protonmail.com>
2024-02-27 00:59:05 -0800

    stop vanilla hud elements from being added to modAll list

--- src/main/java/dev/yurisuika/raised/mixin/client/gui/OverlayRegistryMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/gui/OverlayRegistryMixin.java
@@ -1,21 +1,57 @@
 package dev.yurisuika.raised.mixin.client.gui;
 
+import com.google.common.collect.Lists;
 import net.minecraftforge.client.gui.IIngameOverlay;
 import net.minecraftforge.client.gui.OverlayRegistry;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.List;
 
 import static dev.yurisuika.raised.client.gui.RaisedGui.*;
 
 @Mixin(value = OverlayRegistry.class, remap = false)
 public abstract class OverlayRegistryMixin {
 
+    @Unique
+    private static List<String> vanilla = Lists.newArrayList(
+            "Vignette",
+            "Spyglass",
+            "Helmet",
+            "Frostbite",
+            "Portal",
+            "Hotbar",
+            "Crosshair",
+            "Boss Health",
+            "Player Health",
+            "Armor Level",
+            "Food Level",
+            "Mount Health",
+            "Air Level",
+            "Jump Bar",
+            "Experience Bar",
+            "Item Name",
+            "Sleep Fade",
+            "Text Columns",
+            "FPS Graph",
+            "Potion Icons",
+            "Record",
+            "Subtitles",
+            "Title Text",
+            "Scoreboard",
+            "Chat History",
+            "Player List"
+    );
+
     // MOD ALL BELOW
     @Inject(method = "registerOverlayBottom", at = @At("RETURN"))
     private static void addOverlayBottom(String displayName, IIngameOverlay overlay, CallbackInfoReturnable<IIngameOverlay> cir) {
-        modAll.add(overlay);
+        if (!vanilla.contains(displayName)) {
+            modAll.add(overlay);
+        }
     }
 
     // MOD HUD BELOW
@@ -37,7 +73,9 @@ public abstract class OverlayRegistryMixin {
     // MOD ALL ABOVE
     @Inject(method = "registerOverlayTop", at = @At("RETURN"))
     private static void addOverlayTop(String displayName, IIngameOverlay overlay, CallbackInfoReturnable<IIngameOverlay> cir) {
-        modAll.add(overlay);
+        if (!vanilla.contains(displayName)) {
+            modAll.add(overlay);
+        }
     }
 
 }
