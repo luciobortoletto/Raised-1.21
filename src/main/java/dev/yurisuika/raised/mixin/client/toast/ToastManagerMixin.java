25616ef4353a57b57982947f2619364483544409
yurisuika <yurisuika@protonmail.com>
2024-06-29 13:06:52 -0700

    translate toast entry instead

---
+++ src/main/java/dev/yurisuika/raised/mixin/client/toast/ToastManagerMixin.java
@@ -0,0 +1,36 @@
+package dev.yurisuika.raised.mixin.client.toast;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import dev.yurisuika.raised.util.Translate;
+import dev.yurisuika.raised.util.type.Element;
+import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+public class ToastManagerMixin {
+
+    public abstract static class Toasts {
+
+        @Mixin(targets = "net.minecraft.client.toast.ToastManager$Entry", priority = -999999999)
+        public abstract static class Pre {
+
+            /**
+             * Moves the {@code toasts} if {@link Element.TOASTS} is enabled.
+             */
+            @Inject(method = "draw", at = @At("HEAD"))
+            private void startToastsTranslate(int x, MatrixStack matrixStack, CallbackInfoReturnable<Boolean> cir) {
+                Translate.start(RenderSystem.getModelViewStack(), Element.TOASTS);
+            }
+
+            @Inject(method = "draw", at = @At("TAIL"))
+            private void endToastsTranslate(int x, MatrixStack matrixStack, CallbackInfoReturnable<Boolean> cir) {
+                Translate.end(RenderSystem.getModelViewStack());
+            }
+
+        }
+
+    }
+
+}
