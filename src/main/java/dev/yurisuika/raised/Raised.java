3ac7d4f9449284b79ceae96c9668646032dc079d
yurisuika <yurisuika@protonmail.com>
2024-07-05 00:15:16 -0700

    1.21

--- src/main/java/dev/yurisuika/raised/Raised.java
+++ src/main/java/dev/yurisuika/raised/Raised.java
@@ -2,7 +2,6 @@ package dev.yurisuika.raised;
 
 import dev.yurisuika.raised.client.RaisedOptions;
 import dev.yurisuika.raised.client.commands.RaisedCommand;
-import dev.yurisuika.raised.client.gui.RaisedGui;
 import dev.yurisuika.raised.client.gui.screens.RaisedScreen;
 import dev.yurisuika.raised.util.config.Config;
 import net.minecraft.client.Minecraft;
@@ -14,7 +13,6 @@ import net.minecraftforge.client.event.RegisterKeyMappingsEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod("raised")
 public class Raised {
@@ -38,17 +36,6 @@ public class Raised {
 
     @Mod.EventBusSubscriber(modid = "raised", bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
     public static class ClientModBusEvents {
-
-        @SubscribeEvent
-        public static void clientSetup(FMLClientSetupEvent event) {
-            MinecraftForge.EVENT_BUS.register(new RaisedGui.Hotbar());
-            MinecraftForge.EVENT_BUS.register(new RaisedGui.Chat());
-            MinecraftForge.EVENT_BUS.register(new RaisedGui.Bossbar());
-            MinecraftForge.EVENT_BUS.register(new RaisedGui.Sidebar());
-            MinecraftForge.EVENT_BUS.register(new RaisedGui.Effects());
-            MinecraftForge.EVENT_BUS.register(new RaisedGui.Players());
-            MinecraftForge.EVENT_BUS.register(new RaisedGui.Other());
-        }
 
         @SubscribeEvent
         public static void registerKeyMappings(RegisterKeyMappingsEvent event) {
--- src/main/java/dev/yurisuika/raised/client/gui/components/IconToggleButton.java
+++ src/main/java/dev/yurisuika/raised/client/gui/components/IconToggleButton.java
@@ -15,7 +15,7 @@ public class IconToggleButton extends Button {
     public final int textureWidth;
     public final int textureHeight;
     public boolean toggled;
-    public static final WidgetSprites TEXTURES = new WidgetSprites(new ResourceLocation("widget/button"), new ResourceLocation("widget/button_disabled"), new ResourceLocation("widget/button_highlighted"));
+    public static final WidgetSprites TEXTURES = new WidgetSprites(ResourceLocation.tryParse("widget/button"), ResourceLocation.tryParse("widget/button_disabled"), ResourceLocation.tryParse("widget/button_highlighted"));
 
     public IconToggleButton(int x, int y, int width, int height, Component message, int textureWidth, int textureHeight, ResourceLocation texture, OnPress onPress, CreateNarration createNarration, boolean toggled) {
         super(x, y, width, height, message, onPress, createNarration == null ? DEFAULT_NARRATION : createNarration);
@@ -34,7 +34,7 @@ public class IconToggleButton extends Button {
     }
 
     @Override
-    public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+    public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float delta) {
         if (texture != null) {
             RenderSystem.disableDepthTest();
             guiGraphics.blitSprite(TEXTURES.get(!isToggled(), isHoveredOrFocused()), getX(), getY(), getWidth(), getHeight());
