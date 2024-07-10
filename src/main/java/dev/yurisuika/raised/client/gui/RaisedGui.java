7288ca8f0eb24f71c004469fec27231aae462a78
yurisuika <yurisuika@protonmail.com>
2024-07-09 20:31:02 -0700

    no need to extend

--- src/main/java/dev/yurisuika/raised/client/gui/RaisedGui.java
+++ src/main/java/dev/yurisuika/raised/client/gui/RaisedGui.java
@@ -3,17 +3,11 @@ package dev.yurisuika.raised.client.gui;
 import dev.yurisuika.raised.util.Overlay;
 import dev.yurisuika.raised.util.Translate;
 import dev.yurisuika.raised.util.type.Element;
-import net.minecraft.client.Minecraft;
 import net.neoforged.bus.api.EventPriority;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.neoforge.client.event.RenderGuiOverlayEvent;
-import net.neoforged.neoforge.client.gui.overlay.ExtendedGui;
 
-public class RaisedGui extends ExtendedGui {
-
-    public RaisedGui() {
-        super(Minecraft.getInstance());
-    }
+public class RaisedGui {
 
     public static class Hotbar extends RaisedGui {
 
