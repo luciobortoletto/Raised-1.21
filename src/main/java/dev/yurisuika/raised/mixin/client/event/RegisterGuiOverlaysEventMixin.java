4785607d3d9b479694167e0ac9ed5955c485561c
yurisuika <yurisuika@protonmail.com>
2024-07-10 01:55:22 -0700

    already a resourcelocation

--- src/main/java/dev/yurisuika/raised/mixin/client/event/RegisterGuiOverlaysEventMixin.java
+++ src/main/java/dev/yurisuika/raised/mixin/client/event/RegisterGuiOverlaysEventMixin.java
@@ -2,7 +2,6 @@ package dev.yurisuika.raised.mixin.client.event;
 
 import dev.yurisuika.raised.util.Overlay;
 import net.minecraft.resources.ResourceLocation;
-import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.neoforge.client.event.RegisterGuiOverlaysEvent;
 import net.neoforged.neoforge.client.gui.overlay.IGuiOverlay;
 import org.spongepowered.asm.mixin.Mixin;
@@ -16,61 +15,61 @@ public abstract class RegisterGuiOverlaysEventMixin {
     /**
      * Adds mods registered below all overlays to be moved.
      */
-    @Inject(method = "registerBelowAll", at = @At("HEAD"))
-    private void addOverlayBelowAll(String id, IGuiOverlay overlay, CallbackInfo ci) {
-        Overlay.getOther().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+    @Inject(method = "registerBelowAll(Lnet/minecraft/resources/ResourceLocation;Lnet/neoforged/neoforge/client/gui/overlay/IGuiOverlay;)V", at = @At("HEAD"))
+    private void addOverlayBelowAll(ResourceLocation id, IGuiOverlay overlay, CallbackInfo ci) {
+        Overlay.getOther().add(id);
     }
 
     /**
      * Adds mods registered below certain overlays all to be moved.
      */
-    @Inject(method = "registerBelow", at = @At("HEAD"))
-    private void addOverlayBelow(ResourceLocation other, String id, IGuiOverlay overlay, CallbackInfo ci) {
+    @Inject(method = "registerBelow(Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/resources/ResourceLocation;Lnet/neoforged/neoforge/client/gui/overlay/IGuiOverlay;)V", at = @At("HEAD"))
+    private void addOverlayBelow(ResourceLocation other, ResourceLocation id, IGuiOverlay overlay, CallbackInfo ci) {
         if (Overlay.getHotbar().contains(other)) {
-            Overlay.getHotbar().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getHotbar().add(id);
         } else if (Overlay.getChat().contains(other)) {
-            Overlay.getChat().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getChat().add(id);
         } else if (Overlay.getBossbar().contains(other)) {
-            Overlay.getBossbar().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getBossbar().add(id);
         } else if (Overlay.getSidebar().contains(other)) {
-            Overlay.getSidebar().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getSidebar().add(id);
         } else if (Overlay.getEffects().contains(other)) {
-            Overlay.getEffects().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getEffects().add(id);
         } else if (Overlay.getPlayers().contains(other)) {
-            Overlay.getPlayers().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getPlayers().add(id);
         } else if (Overlay.getToasts().contains(other)) {
-            Overlay.getToasts().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getToasts().add(id);
         }
     }
 
     /**
      * Adds mods registered above certain overlays all to be moved.
      */
-    @Inject(method = "registerAbove", at = @At("HEAD"))
-    private void addOverlayAbove(ResourceLocation other, String id, IGuiOverlay overlay, CallbackInfo ci) {
+    @Inject(method = "registerAbove(Lnet/minecraft/resources/ResourceLocation;Lnet/minecraft/resources/ResourceLocation;Lnet/neoforged/neoforge/client/gui/overlay/IGuiOverlay;)V", at = @At("HEAD"))
+    private void addOverlayAbove(ResourceLocation other, ResourceLocation id, IGuiOverlay overlay, CallbackInfo ci) {
         if (Overlay.getHotbar().contains(other)) {
-            Overlay.getHotbar().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getHotbar().add(id);
         } else if (Overlay.getChat().contains(other)) {
-            Overlay.getChat().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getChat().add(id);
         } else if (Overlay.getBossbar().contains(other)) {
-            Overlay.getBossbar().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getBossbar().add(id);
         } else if (Overlay.getSidebar().contains(other)) {
-            Overlay.getSidebar().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getSidebar().add(id);
         } else if (Overlay.getEffects().contains(other)) {
-            Overlay.getEffects().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getEffects().add(id);
         } else if (Overlay.getPlayers().contains(other)) {
-            Overlay.getPlayers().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getPlayers().add(id);
         } else if (Overlay.getToasts().contains(other)) {
-            Overlay.getToasts().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+            Overlay.getToasts().add(id);
         }
     }
 
     /**
      * Adds mods registered above all overlays to be moved.
      */
-    @Inject(method = "registerAboveAll", at = @At("HEAD"))
-    private void addOverlayAboveAll(String id, IGuiOverlay overlay, CallbackInfo ci) {
-        Overlay.getOther().add(ResourceLocation.tryParse(ModLoadingContext.get().getActiveNamespace() + ":" + id));
+    @Inject(method = "registerAboveAll(Lnet/minecraft/resources/ResourceLocation;Lnet/neoforged/neoforge/client/gui/overlay/IGuiOverlay;)V", at = @At("HEAD"))
+    private void addOverlayAboveAll(ResourceLocation id, IGuiOverlay overlay, CallbackInfo ci) {
+        Overlay.getOther().add(id);
     }
 
 }
