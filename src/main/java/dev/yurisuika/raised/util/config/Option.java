51a85f60360d0af8329bb4e04be2c4102f3fb6e0
yurisuika <yurisuika@protonmail.com>
2024-07-11 00:11:26 -0700

    use method for return

--- src/main/java/dev/yurisuika/raised/util/config/Option.java
+++ src/main/java/dev/yurisuika/raised/util/config/Option.java
@@ -73,7 +73,7 @@ public class Option {
     }
 
     public static Texture getTexture() {
-        return getToggle().texture;
+        return getToggle().getTexture();
     }
 
     public static void setTexture(Texture value) {
